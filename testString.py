af2280d3c0d4afe572c730f39de62343c5134336
butcher <dong1008313@163.com>
2018-05-27 09:52:45 +0800

    string

---
+++ testString.py
@@ -0,0 +1,24 @@
+print()
+
+
+# list1 = ["syr","sdf",'"sdff"','sdfas']
+# print(list1)
+#
+# for i in list1:
+#     print(i + '--->')
+#
+# print(list1)
+
+list2 = [1122,'sdfsdf',True,1122,"apple"]
+# print(list2)
+# for n in list2:
+#     print(n)
+#     print(type(n))
+
+# list2.remove(1122)
+
+# list2[1] = "aaaaa"
+
+print(1122 in list2)
+
+print(list2)
